a438881ed202d0a57cc5ac2b5077d04bc1f3fa75
TheMrKan <mrkan2017@gmail.com>
2025-02-04 17:09:50 +0400

    отключено сжатие и версионирование статических файлов

--- HorizonGlow/settings.py
+++ HorizonGlow/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import logging.config
 
+import whitenoise.storage
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -228,7 +230,7 @@ STORAGES = {
         }
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     }
 }
 WHITENOISE_MAX_AGE = 86400
